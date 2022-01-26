3b292e12e5368c4baf93e4f0b9aad4768879c8c7
TinGoll <roma.oliynyk83@gmail.com>
2022-01-26 11:24:01 +0300

    Commit 26.01.2022 v1.1

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import Engine from "./engine/Engine";
+import Entity from "./Models/entities/Entity";
 import { EntityOptions, EntityComponents, GeometryComponent, FinishingComponent, PriceComponent, APIEntity } from "./types/entity-types";
 import { EntityType } from "./utils/entity-units";
 
 export default Engine;
+
+export {
+    Entity
+}
+
 export {
     EntityOptions, 
     EntityComponents, 
