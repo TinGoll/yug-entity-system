2ded06fe896b23ea781d51ec579e3160abd51cd9
TinGoll <roma.oliynyk83@gmail.com>
2022-03-09 17:04:02 +0300

    Update 1.1.0 v2

--- src/Models/entities/EntityProduct.ts
+++
@@ -1,8 +0,0 @@
-import { EntityOptions } from "../../types/entity-types";
-import Entity from "./Entity";
-
-export class EntityProduct extends Entity {
-    constructor(options: EntityOptions) {
-        super(options);
-    }
-}
