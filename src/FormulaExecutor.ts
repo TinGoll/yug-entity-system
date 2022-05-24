7ba7f8360ec49860351e95cd8605b555c4ffd24e
TinGoll <roma.oliynyk83@gmail.com>
2022-05-24 13:04:59 +0300

    Commit 24.05.2022 v1.6

--- src/FormulaExecutor.ts
+++ src/FormulaExecutor.ts
@@ -288,6 +288,9 @@ export function formulaExecutor(this: Entity, { componentName, propertyName, pro
                 if (father?.key === entity.key) THIS_IS = "father"; // +
                 if (grand_father?.key === entity.key) THIS_IS = "grand_father"; // +
 
+                console.log("THIS_IS", THIS_IS);
+                
+
                 if (!THIS_IS) continue;
                 const KEY = `${THIS_IS}-${cmp.componentName}-${cmp.propertyName}`.toLocaleLowerCase(); // +
 
